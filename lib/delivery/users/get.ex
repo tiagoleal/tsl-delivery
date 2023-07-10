b1ee08f59dc17c72e77bd7da41102bc2fc9d6d56
Tiago Leal <tiago.sleal12@gmail.com>
2023-07-10 16:15:14 -0300

    feat: add plug connection before conection

--- lib/delivery/users/get.ex
+++ lib/delivery/users/get.ex
@@ -1,17 +1,7 @@
 defmodule Delivery.Users.Get do
-  alias Ecto.UUID
   alias Delivery.{Error, Repo, User}
 
   def by_id(id) do
-    case UUID.cast(id) do
-      # :error -> {:error, %{status: :bad_request, result: "Invalid id format!"}}
-      :error -> {:error, Error.build_id_format_error() }
-      {:ok, uuid} ->  get(uuid)
-    end
-  end
-
-
-  defp get(id) do
     case Repo.get(User, id) do
       # nil -> {:error, %{status: :not_found, result: "User not found!"}}
       nil -> {:error, Error.build_user_not_found_error()}
